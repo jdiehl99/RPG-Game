4c42feb2225a0774848e6c67636447c6ecbf5cdf
Jodi Diehl <jdiehl99@gmail.com>
2017-08-27 11:49:12 -0400

    added some graphics and sound effects and did pseudo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+// assign health points to each fighter
+// assign attack points to each fighter
+// upon selection of fighter, move remaining 3 fighters to "enemies available to attack" area
+// upon selection of defender, move selected defender to "defender" area
+// when attack button is pressed, reduce HP on defender & attacker per original values
+// on each subsequent attack, attacker attack points will increase by starting number (ie: attack 2 = 4+4 if starting at 4)
+// defender attack points do not change
+// game continues until attacker or defender reaches zero HP
+// if defender wins, game is over
+// if attacker wins, defender disappears and player may select new defender
+// upon pressing attack, previous attack store for player is used and continues to build incrementally
+// game continues until player has lost all HP or all defenders have been defeated
+// upon game end all fighters are reset to original locations and all attack/HP points are reset to original values after pressing reset button
+// if player presses attack with no defender selected, show error message
+
+// Mama Murphy 120 / 8
+// Vault-Tec 100 / 5
+// Piper 150 / 20
+// Maccready 180 / 25 
